10ab5e5ef7e76de5abdc14f5ee2fc1b901bab43d
Matt Powley <mattpow35@gmail.com>
2016-10-03 07:58:01 -0600

    linked the added view controller with updated view controller class

---
+++ PrepForAboutMe/Controller/UpdatedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpdatedViewController.swift
+//  PrepForAboutMe
+//
+//  Created by Powley, Matthew on 10/3/16.
+//  Copyright © 2016 Powley, Matthew. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    override func viewDidLoad()
+    {
+        //load the parent versin of a view.
+        super.viewDidLoad()
+        //Load this pages components.
+    }
+}
